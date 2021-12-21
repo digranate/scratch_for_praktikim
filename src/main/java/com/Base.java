3af56e942b82e504b4092b36c6e1ad1de12358a0
atrif <napoyumolocom2013@gmail.com>
2021-12-21 20:28:32 +0300

    change base url

--- src/main/java/com/Base.java
+++ src/main/java/com/Base.java
@@ -5,7 +5,7 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.http.ContentType.JSON;
 
 public class Base {
-    public static final String BASE_URL = "https://norma.nomoreparties.space/api/";
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/api/";
 
     public static RequestSpecification getBaseSpec() {
         return new RequestSpecBuilder()
